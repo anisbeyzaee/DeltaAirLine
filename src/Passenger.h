16fd6dddab952ec43ff4d3ceb362fc8b530e71ef
anisbeyzaee <32579447+anisbeyzaee@users.noreply.github.com>
2017-11-11 12:34:04 -0800

    Update Passenger.h

--- src/Passenger.h
+++ src/Passenger.h
@@ -19,8 +19,6 @@ private:
 	string lastName;
 	string phoneNumber;
 	string address;
-
-
 public:
 	Passenger(); // default Constructor
 	Passenger(string firstName, string lastName, string phoneNumber, string address);
@@ -29,6 +27,8 @@ public:
  void setLastName(string lastName);
  void setPhoneNumber(string phoneNumber);
  void setAddress(string address);
+ void printPassenger(const Passenger &) ;
+ void searchPassenger(string firsName, string lastName);
   bool operator>(const Passenger &) const;       // is object > parameter?
   bool operator<(const Passenger &) const;       // is object < parameter?
   bool operator==(const Passenger &) const;      // is object == parameter?
@@ -36,9 +36,8 @@ string getFirstName()const;
 string getLastName() const;
 string getPhoneNumber() const;
 string getAddress() const;
+
+friend ostream & operator << (ostream & os, Passenger & p);
 ~ Passenger();
 };
 
-
-
-#endif /* PASSENGER_H_ */
