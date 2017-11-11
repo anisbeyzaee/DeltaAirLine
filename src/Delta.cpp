6e8bc34d567e4b443ed564dce86424a7ce4e07ca
anisbeyzaee <32579447+anisbeyzaee@users.noreply.github.com>
2017-11-11 12:45:43 -0800

    Update Delta.cpp

--- src/Delta.cpp
+++ src/Delta.cpp
@@ -9,18 +9,13 @@
 #include <iostream>
 #include <string>
 #include<fstream>
-
+#include"TravelBookingSystem.h"
 #include "OrderedLinkedList.h"
 #include "Passenger.h"
 using namespace std;
 
-
-
 string header="A(Add) | S (Search) | D(Delete) |L(List) |Q(Quit):   ";
-
-
 void print_seperator();
-
 bool checkFlightNumber(int a);
 char choice;	//  user input
 string name;
@@ -31,29 +26,36 @@ int flightnumber;
 bool trigger=1;
 
 int main(){
-
+    TravelBookingSystem bookSys;
 	while (trigger) {
 		cout<<header<< endl;
 		cin>>choice;
 		switch(choice){
 						case 'A':
 						case 'a':
-							cout<< "Enter your flight number\n"; cin>> flightnumber;
+							cout<< "Enter your flight number (100, 200, 300 or 400)\n"; cin>> flightnumber;
 							if (checkFlightNumber(flightnumber)){
 							cout << "Enter a Name \n"; cin >> name;
 							cout << "Enter SurName \n"; cin >> lastname;
-							cout << "Enter Number \n"; cin >> phonenumber;
-							cout << "Enter your address.."; cin>> address;
-							Passenger p(name, lastname, phonenumber, address);
+							cout << "Enter Pnone Number \n"; cin >> phonenumber;
+							cout << "Enter your address.."; cin>> address;\
+
+						    Passenger p(name, lastname, phonenumber, address) ;
+						    bookSys.addPassenger(flightnumber,p);
+						    cout<< "Delta.cpp line 51"<< p.getFirstName()<<p.getLastName()<< endl; // debugging statement
+						    cout<<"Delta.cpp line 52"<< endl;										// debugging statement
 							}
 							else{
 								cout << "Invalid flight Number";
+//								cout<<header<< endl;
+//										cin>>choice;
 							}
 							break;
 						case 'S':
 						case 's':
 							cout << "Enter a Name \n"; cin >> name;
 							cout << "Enter SurName \n"; cin >> lastname;
+							bookSys.search(name, lastName);  // to be searched by Passenger p in OrderedLinkedList or  name /last name in passenger
 							break;
 						case 'D':
 						case 'd':
@@ -64,14 +66,15 @@ int main(){
 							break;
 						case 'L':
 						case 'l':
-							cout<< "Enter your flight number\n"; cin>> flightnumber;
 
+							cout<< "Enter your flight number (100, 200, 300 or 400)\n"; cin>> flightnumber;
+							bookSys.listPassenger(flightnumber);
 
 							break;
 
 						case 'Q':
 						case 'q':
-							cout<<"Thanks for using the PhoneList";
+							cout<<"Thanks for using the Delta Air Line Booking System";
 							trigger=0;
 							break;
 						default:
@@ -88,10 +91,16 @@ int main(){
 void print_seperator(){
 	cout << "-------------------------------------- \n";
 }
-// This function checks the flight number to be valid
+// This function checks the flight number is valid (100, 200, 300, 400)
 bool checkFlightNumber(int number){
-		if(number==100 || 200 || 300 || 400){
-		return true;
-		}else{
-	return false;}
+	 int checklist[4]= { 100, 200, 300, 400};
+	 for (int i=0; i<4; i++){
+		 if (checklist[i] == number){
+			 return true;
+		//if(number%100 && 99< number && number< 401){
+		break;
+
+		}
+}
+return false;
 }
